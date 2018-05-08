e3762a94e509a9e435d5534d799276c564cf549d
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2018-05-08 16:16:47 +0200

    add fallback whitelist to torset

---
+++ torset_add_fbdirs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+cat external/fallback.whitelist | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' | tr ':' ',' | while read entry; do ipset add torset $entry; done
