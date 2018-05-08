9055462a8472696a0ac022546f2b564451bee4fe
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2018-05-08 16:26:16 +0200

    dont use set -e when adding to ipset
    but we should actually fix the duplicates

--- torset_add_guards.sh
+++ torset_add_guards.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-set -e
-
 cat consensus | grep -B 2 Guard | grep -o '[0-9]\{2,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\ [0-9]\{1,5\}' | tr ' ' ',' | while read entry; do ipset add torset $entry; done
