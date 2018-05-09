c1d72bdb2e925bbc2e4bdab4ce3eb1701555950b
Martin Kepplinger <martink@posteo.de>
2018-05-09 14:33:33 +0200

    download_consensus: add 10s timeout

--- download_consensus.sh
+++ download_consensus.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat external/auth_dirs.inc | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | while read entry; do wget $entry/tor/status-vote/current/consensus && break ; done
+cat external/auth_dirs.inc | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | while read entry; do wget --timeout=10 $entry/tor/status-vote/current/consensus && break ; done
