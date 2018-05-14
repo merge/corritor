2438a30d61e0f100180f714fb98cfac79c9167f8
Martin Kepplinger <martink@posteo.de>
2018-05-14 12:26:37 +0200

    torset_add_fbdirs: remove duplicate entries before adding

--- torset_add_fbdirs.sh
+++ torset_add_fbdirs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cat external/fallback.whitelist | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' | tr ':' ',' | while read entry; do ipset add torset $entry; done
+set -e
+
+cat external/fallback.whitelist | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' | tr ':' ',' | sort -u | while read entry; do ipset add torset $entry; done
